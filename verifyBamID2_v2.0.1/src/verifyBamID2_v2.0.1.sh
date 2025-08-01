db3da0cd5080fab8223a0006dc55897f9a1f3616
Caitlin <caitlin.blundell@synnovis.co.uk>
2025-08-01 11:11:42 +0100

    svd and docker fix

--- verifyBamID2_v2.0.1/src/verifyBamID2_v2.0.1.sh
+++ verifyBamID2_v2.0.1/src/verifyBamID2_v2.0.1.sh
@@ -19,10 +19,12 @@ if [ "$skip" != "true" ]; then
     # Get input filenames as strings
 
     reference_fasta_name=$(dx describe --name "$reference_fasta")
+    reference_fasta_index_name=$(dx describe --name "$reference_fasta_index")
     input_bam_name=$(dx describe --name "$input_bam")
     input_bam_index_name=$(dx describe --name "$input_bam_index")
-    svd_prefix_name=$(dx describe --name "$svd_prefix")
 
+    # Set path to svd files
+    svd_prefix_path="/home/dnanexus/svd/${svd_prefix}"
 
     # Create variable for output filename by removing .bam extension
     bam_prefix="${input_bam_name%.bam}"
@@ -31,17 +33,42 @@ if [ "$skip" != "true" ]; then
 
     dx-download-all-inputs --parallel
 
+    # Copy bai into the input_bam directory
+    
+    cp /home/dnanexus/in/input_bam_index/"$input_bam_index_name" /home/dnanexus/in/input_bam/"${input_bam_name}.bai"
+
+    # Decompress reference fasta if it is gzipped
+    if [[ "$reference_fasta_name" == *.gz ]]; then
+        gunzip -c /home/dnanexus/in/reference_fasta/"$reference_fasta_name" > /home/dnanexus/in/reference_fasta/"${reference_fasta_name%.gz}"
+        reference_fasta_name="${reference_fasta_name%.gz}"
+    fi
+
+
+    # Unpack fasta-index files
+    mkdir -p /home/dnanexus/in/reference_fasta_index_extracted
+    tar -xzvf /home/dnanexus/in/reference_fasta_index/"$reference_fasta_index_name" \
+        -C /home/dnanexus/in/reference_fasta_index_extracted
+
+    # Move files into the same directory as the reference FASTA
+    mv /home/dnanexus/in/reference_fasta_index_extracted/* /home/dnanexus/in/reference_fasta/
+    
+
     # Create output directories
     mkdir -p /home/dnanexus/out/selfSM
     mkdir -p /home/dnanexus/out/ancestry
 
-    # Safely decompress the Docker tarball
+    
+   ## Unpack the saved Docker image tarball into a .tar file
     gunzip -c /home/dnanexus/verifybamid2.tar.gz > /home/dnanexus/verifybamid2.tar
 
+# Load the Docker image
+    DOCKERIMAGENAME=$(
+        docker load < /home/dnanexus/verifybamid2.tar \
+        | grep "Loaded image:" \
+        | cut -d' ' -f3
+    )
 
-    # Load the Docker image and capture the image name
-    DOCKERIMAGENAME=$(docker load < /home/dnanexus/verifybamid2.tar | \
-        grep "Loaded image:" | cut -d' ' -f3)
+    # Remove the .tar to save space
     rm /home/dnanexus/verifybamid2.tar
 
 
@@ -50,8 +77,7 @@ if [ "$skip" != "true" ]; then
     docker run -v /home/dnanexus:/home/dnanexus/ \
         --rm \
         "$DOCKERIMAGENAME" \
-        VerifyBamID2 \
-        --SVDPrefix /home/dnanexus/in/svd_prefix/"$svd_prefix_name" \
+        --SVDPrefix "$svd_prefix_path" \
         --Reference /home/dnanexus/in/reference_fasta/"$reference_fasta_name" \
         --BamFile /home/dnanexus/in/input_bam/"$input_bam_name" \
         --Output /home/dnanexus/out/"$bam_prefix" \
