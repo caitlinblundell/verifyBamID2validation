86ea33852b866373784240133b88d05bb7a86fe2
Caitlin <caitlin.blundell@synnovis.co.uk>
2026-02-03 09:32:08 +0000

    change to run as batch run

--- testing_scripts/generate_dx_commands.sh
+++ testing_scripts/generate_dx_commands.sh
@@ -1,31 +1,27 @@
 # Generates commands to copy samples into 003 validation project and run verifyBAMID on them
 # Usage: bash generate_dx_commands.sh <source_project>
 
-# get source project id from command line argument
+# Get source project id from command line argument
 SOURCE_PROJECT=$1
 VALIDATION_PROJECT=project-J0yfQ400Jy1pJP08gFkGq6q7
 
-# get list of BAM files in source project using find data
-BAM_FILES=$(dx find data --path $SOURCE_PROJECT:/output --name "*.bam" --norecurse --brief)
+# Count number of BAM files in source project
+BAM_FILES=$(dx find data --project $SOURCE_PROJECT:/output/ --name "*.bam" --norecurse --brief)
 echo "Number of BAM files found: $(echo "$BAM_FILES" | wc -l)"
 
-# move to validation project
+# Move into validation project
 dx select $VALIDATION_PROJECT
-dx mkdir /cp2_samples
 
-# copy each BAM file to validation project with its corresponding index file, and add to batch file
-for BAM_FILE in $BAM_FILES; do
-    # Get filename from file id
-    BAM_NAME=$(dx describe "$BAM_FILE" --name)
+# Generate batch TSV
+BATCH_FILE="verifyBAMID_batch.tsv"
+dx generate_batch_inputs \
+--path $SOURCE_PROJECT:/output/ \
+-o $BATCH_FILE \
+-i input_bam="*.bam" \
+-i input_bam_index="*.bam.bai" \
 
-    # Copy files
-    dx cp $SOURCE_PROJECT:$BAM_FILE $VALIDATION_PROJECT:/cp2_samples/
-    dx cp $SOURCE_PROJECT:/output/${BAM_NAME}.bai $VALIDATION_PROJECT:/cp2_samples/
-
-    # Create batch TSV file to run as batch job
-    BATCH_FILE="verifyBAMID_batch.tsv"
-    echo -e "input_bam\tinput_bam_index\tskip" > $BATCH_FILE
-
-    # run verifyBAMID applet on each BAM file in validation project
-    dx run project-ByfFPz00jy1fk6PjpZ95F27J:applet-J5KPy2Q0Jy1YFBxGZJbxpjPY --name "verifyBAMID_${BAM_NAME%.bam}" --destination $VALIDATION_PROJECT:/cp2_samples/ -iinput_bam=$BAM_NAME -iinput_bam_index=${BAM_NAME}.bai -iskip=false
-done
+# Run verifyBAMID applet on each BAM file in validation project as batch run
+dx run project-ByfFPz00jy1fk6PjpZ95F27J:applet-J5KPy2Q0Jy1YFBxGZJbxpjPY \
+--name "verifyBAMID_batchrun" \
+--destination $VALIDATION_PROJECT:/cp2_samples/ \
+--batch-tsv $BATCH_FILE
